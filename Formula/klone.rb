d7ba31a571b8f5f6b13ee21b72c5ff6a9e907b11
Pavel Tetayev <pahanini@gmail.com>
2025-04-03 15:08:53 +0500

    Fix hash

--- Formula/klone.rb
+++ Formula/klone.rb
@@ -2,7 +2,7 @@ class Klone < Formula
   desc "Tool for fast domain/username/repo folder structure repo cloning"
   homepage "https://github.com/pahanini/klone"
   url "https://github.com/pahanini/klone/releases/download/v0.0.4/klone.arm64.tar.gz"
-  sha256 "dadfb80b81c221bb1a55cc54a2492abf9ddc0f81271f8ecfa81d459b26ee4490"
+  sha256 "303a4ad8117bf5e9cff839809f2382c592a090ae289166aab603559c43327a6f"
   license "MIT"
   version "0.0.4"
 
