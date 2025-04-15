c6b827275919642f59a6c713afe8bcd23103e027
Pavel Tetayev <pahanini@gmail.com>
2025-04-15 19:10:12 +0500

    remove version

--- Formula/klone.rb
+++ Formula/klone.rb
@@ -1,7 +1,7 @@
 class Klone < Formula
   desc "Tool for fast domain/username/repo folder structure repo cloning"
   homepage "https://github.com/pahanini/klone"
-  url "https://github.com/your-org/your-repo/archive/refs/tags/.tar.gz"
+  url "https://github.com/your-org/your-repo/archive/refs/tags/v0.0.5.tar.gz"
   sha256 "303a4ad8117bf5e9cff839809f2382c592a090ae289166aab603559c43327a6f"
   license "MIT"
 
